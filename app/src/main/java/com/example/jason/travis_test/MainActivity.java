be86009152bf242f04a18dfd97a729745bafc80e
ericwhrz <ericwhrz@gmail.com>
2017-10-24 00:18:18 +0100

    [EWR] Changed MainActivity.java

--- app/src/main/java/com/example/jason/travis_test/MainActivity.java
+++ app/src/main/java/com/example/jason/travis_test/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Why");
+        System.out.println("I love fidget spinners");
     }
 }
